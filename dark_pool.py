6bbd1a2380750cdc3f4fca80c3ec0c8d3a3ea87b
George <gc14768@my.bristol.ac.uk>
2019-03-24 12:55:23 +0000

    Removed excessive indentation from the Trader class

--- dark_pool.py
+++ dark_pool.py
@@ -274,73 +274,73 @@ class Exchange(Orderbook):
 # all Traders have a trader id, bank balance, blotter, and list of orders to execute
 class Trader:
 
-        def __init__(self, ttype, tid, balance, time):
-                self.ttype = ttype             # what type / strategy this trader is
-                self.tid = tid                 # trader unique ID code
-                self.balance = balance         # money in the bank
-                self.blotter = []              # record of trades executed
-                self.customer_order = None     # customer orders currently being worked (fixed at 1)
-                self.n_quotes = 0              # number of quotes live on LOB
-                self.willing = 1               # used in ZIP etc
-                self.able = 1                  # used in ZIP etc
-                self.birthtime = time          # used when calculating age of a trader/strategy
-                self.profitpertime = 0         # profit per unit time
-                self.n_trades = 0              # how many trades has this trader done?
-                self.lastquote = None          # record of what its last quote was
+    def __init__(self, ttype, tid, balance, time):
+        self.ttype = ttype             # what type / strategy this trader is
+        self.tid = tid                 # trader unique ID code
+        self.balance = balance         # money in the bank
+        self.blotter = []              # record of trades executed
+        self.customer_order = None     # customer orders currently being worked (fixed at 1)
+        self.n_quotes = 0              # number of quotes live on LOB
+        self.willing = 1               # used in ZIP etc
+        self.able = 1                  # used in ZIP etc
+        self.birthtime = time          # used when calculating age of a trader/strategy
+        self.profitpertime = 0         # profit per unit time
+        self.n_trades = 0              # how many trades has this trader done?
+        self.lastquote = None          # record of what its last quote was
 
 
-        def __str__(self):
-                return '[TID %s type %s balance %s blotter %s customer order %s n_trades %s profitpertime %s]' \
-                       % (self.tid, self.ttype, self.balance, self.blotter, self.customer_order, self.n_trades, self.profitpertime)
+    def __str__(self):
+        return '[TID %s type %s balance %s blotter %s customer order %s n_trades %s profitpertime %s]' \
+            % (self.tid, self.ttype, self.balance, self.blotter, self.customer_order, self.n_trades, self.profitpertime)
 
 
-        def add_order(self, customer_order, verbose):
-                # in this version, trader has at most one order,
-                # if allow more than one, this needs to be self.orders.append(order)
-                if self.n_quotes > 0 :
-                    # this trader has a live quote on the LOB, from a previous customer order
-                    # need response to signal cancellation/withdrawal of that quote
-                    response = 'LOB_Cancel'
-                else:
-                    response = 'Proceed'
-                self.customer_order = customer_order
-                if verbose : print('add_order < response=%s' % response)
-                return response
+    def add_order(self, customer_order, verbose):
+        # in this version, trader has at most one order,
+        # if allow more than one, this needs to be self.orders.append(order)
+        if self.n_quotes > 0 :
+            # this trader has a live quote on the LOB, from a previous customer order
+            # need response to signal cancellation/withdrawal of that quote
+            response = 'LOB_Cancel'
+        else:
+            response = 'Proceed'
+        self.customer_order = customer_order
+        if verbose : print('add_order < response=%s' % response)
+        return response
 
 
-        def del_order(self, order):
-                # this is lazy: assumes each trader has only one customer order with quantity=1, so deleting sole order
-                # CHANGE TO DELETE THE HEAD OF THE LIST AND KEEP THE TAIL
-                self.customer_order = None
+    def del_order(self, order):
+        # this is lazy: assumes each trader has only one customer order with quantity=1, so deleting sole order
+        # CHANGE TO DELETE THE HEAD OF THE LIST AND KEEP THE TAIL
+        self.customer_order = None
 
 
-        def bookkeep(self, trade, verbose):
+    def bookkeep(self, trade, verbose):
 
-                outstr=str(self.customer_order)
+        outstr=str(self.customer_order)
 
-                self.blotter.append(trade)  # add trade record to trader's blotter
-                # NB What follows is **LAZY** -- assumes all orders are quantity=1
-                transactionprice = trade['price']
-                if self.customer_order.otype == 'Buy':
-                        profit = (self.customer_order.price - transactionprice) * trade['quantity']
-                else:
-                        profit = (transactionprice - self.customer_order.price) * trade['quantity']
-                self.balance += profit
-                self.n_trades += 1
-                self.profitpertime = self.balance/(trade['time'] - self.birthtime)
+        self.blotter.append(trade)  # add trade record to trader's blotter
+        # NB What follows is **LAZY** -- assumes all orders are quantity=1
+        transactionprice = trade['price']
+        if self.customer_order.otype == 'Buy':
+            profit = (self.customer_order.price - transactionprice) * trade['quantity']
+        else:
+            profit = (transactionprice - self.customer_order.price) * trade['quantity']
+        self.balance += profit
+        self.n_trades += 1
+        self.profitpertime = self.balance/(trade['time'] - self.birthtime)
 
-                if verbose: print('%s profit=%d balance=%d profit/time=%d' % (outstr, profit, self.balance, self.profitpertime))
+        if verbose: print('%s profit=%d balance=%d profit/time=%d' % (outstr, profit, self.balance, self.profitpertime))
 
 
-        # specify how trader responds to events in the market
-        # this is a null action, expect it to be overloaded by specific algos
-        def respond(self, time, lob, trade, verbose):
-                return None
+    # specify how trader responds to events in the market
+    # this is a null action, expect it to be overloaded by specific algos
+    def respond(self, time, lob, trade, verbose):
+        return None
 
-        # specify how trader mutates its parameter values
-        # this is a null action, expect it to be overloaded by specific algos
-        def mutate(self, time, lob, trade, verbose):
-                return None
+    # specify how trader mutates its parameter values
+    # this is a null action, expect it to be overloaded by specific algos
+    def mutate(self, time, lob, trade, verbose):
+        return None
 
 
 # Trader subclass Giveaway
